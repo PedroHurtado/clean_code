a4ed7dbf7eeadd9979915c9626b575596d95afc1
Pedro Hurtado <pedrohurt@gmail.com>
2025-09-22 12:03:27 +0200

    strategy

---
+++ tema1_todo_codigo_es_mejorable/strategy-pattern.js
@@ -0,0 +1,23 @@
+const OPERATIONS = Object.freeze({
+    ADD: (a, b) => a + b,
+    SUBTRACT: (a, b) => a - b,
+    MULTIPLY: (a, b) => a * b,
+    DIVIDE: (a, b) => {
+        if (b === 0) {
+            throw new Error('Division by zero is not allowed');
+        }
+        return a / b
+    }
+});
+
+function calculateBasicOperation(firstOperand, secondOperand, operation) {
+
+    const operation = OPERATIONS[operation]
+    if (!operation) {
+        throw new Error(`Unknown operation: ${operation}`);
+    }
+    return operation(firstOperand, secondOperand)
+}
+
+//OPERATIONS.ADD = null ERROR
+OPERATIONS.ADD = ()=>{} //NO DA ERROR Pero no se modifica el objeto
